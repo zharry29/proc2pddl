47e9a9ad6e7f2485bfb4c5cf451d0bb8802d61e4
Harry Zhang <zharry@seas.upenn.edu>
2023-06-02 10:55:43 -0700

    Change folder names

--- pddl_evaluation/src/make_gold.py
+++ pddl_evaluation/src/make_gold.py
@@ -8,16 +8,17 @@ for folder_name in os.listdir("../../pddl_data"):
     # Copy domain files
     with open(f"../../pddl_data/{folder_name}/domain.pddl", "r") as f:
         domain = f.read()
-    Path(f"../data/evaluation/actions_generation/true/{folder_name}").mkdir(parents=True, exist_ok=True)
-    with open(f"../data/evaluation/actions_generation/true/{folder_name}/domain.pddl", "w") as f:
+    stripped_folder_name = folder_name.strip("submission_")
+    Path(f"../data/evaluation/actions_generation/true/{stripped_folder_name}").mkdir(parents=True, exist_ok=True)
+    with open(f"../data/evaluation/actions_generation/true/{stripped_folder_name}/domain.pddl", "w") as f:
         f.write(domain)
-    with open(f"../data/evaluation/actions_generation/true/{folder_name}/domain_header.pddl", "w") as f:
+    with open(f"../data/evaluation/actions_generation/true/{stripped_folder_name}/domain_header.pddl", "w") as f:
         f.write(get_domain_header(domain))
     # Copy problem files
-    Path(f"../data/evaluation/actions_generation/true/{folder_name}/problems").mkdir(parents=True, exist_ok=True)
+    Path(f"../data/evaluation/actions_generation/true/{stripped_folder_name}/problems").mkdir(parents=True, exist_ok=True)
     for fname in os.listdir(f"../../pddl_data/{folder_name}"):
         if fname.startswith("problem-"):
             with open(f"../../pddl_data/{folder_name}/{fname}", "r") as f:
                 problem = f.read()
-            with open(f"../data/evaluation/actions_generation/true/{folder_name}/problems/{fname}", "w") as f:
+            with open(f"../data/evaluation/actions_generation/true/{stripped_folder_name}/problems/{fname}", "w") as f:
                 f.write(problem)
